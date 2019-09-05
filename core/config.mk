11d88c017823ea7f998bf794ec292b187005af83
dianlujitao <dianlujitao@lineageos.org>
2019-09-05 20:39:07 +0800

    core: Include our vendor config

--- core/config.mk
+++ core/config.mk
@@ -1176,4 +1176,6 @@ endif
 DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
+include vendor/hentai/build/core/config.mk
+
 include $(BUILD_SYSTEM)/dumpvar.mk
